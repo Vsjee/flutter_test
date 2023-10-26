9ba08f9e3e6818f4ee700a93b92eb7176e01e3d8
David Hernandez <davidfelipehernandez1@gmail.com>
2023-10-26 18:23:45 -0500

    fix router add shell routes

--- lib/main.dart
+++ lib/main.dart
@@ -3,22 +3,52 @@ import 'package:go_router/go_router.dart';
 import 'package:test/pages/categories/categories_screen.dart';
 import 'package:test/pages/home/home_screen.dart';
 import 'package:test/pages/profile/profile_screen.dart';
+import 'package:test/shared/widgets/bottom_nav_bar.dart';
 
 // GoRouter configuration
 GoRouter _router = GoRouter(
-  routes: <GoRoute>[
-    GoRoute(
-      path: '/',
-      builder: (context, state) => const HomeScreen(),
+  routes: [
+    ShellRoute(
+      builder: (BuildContext context, GoRouterState state, Widget child) {
+        return Scaffold(
+          appBar: AppBar(title: const Text('App Shell')),
+          body: Center(
+            child: child,
+          ),
+          bottomNavigationBar: const BottomNavBar(),
+        );
+      },
+      routes: [
+        GoRoute(
+            path: '/',
+            builder: (BuildContext context, GoRouterState state) {
+              return const HomeScreen();
+            }),
+        GoRoute(
+            path: '/categories',
+            builder: (BuildContext context, GoRouterState state) {
+              return const CategoriesScreen();
+            }),
+        GoRoute(
+            path: '/profile',
+            builder: (BuildContext context, GoRouterState state) {
+              return const ProfileScreen();
+            }),
+      ],
     ),
-    GoRoute(
-      path: '/categories',
-      builder: (context, state) => const CategoriesScreen(),
-    ),
-    GoRoute(
-      path: '/profile',
-      builder: (context, state) => const ProfileScreen(),
-    ),
+    // GoRoute(
+    //     path: '/',
+    //     builder: (context, state) => const HomeScreen(),
+    //     routes: <GoRoute>[
+    //       GoRoute(
+    //         path: 'categories',
+    //         builder: (context, state) => const CategoriesScreen(),
+    //       ),
+    //       GoRoute(
+    //         path: 'profile',
+    //         builder: (context, state) => const ProfileScreen(),
+    //       ),
+    //     ]),
   ],
 );
 
