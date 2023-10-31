58e5ce9cd2ad396ddcc4aad5c4b182b6583e01ba
David Hernandez <davidfelipehernandez1@gmail.com>
2023-10-31 08:48:41 -0500

    remove unused code in home screen

--- lib/pages/home/home_screen.dart
+++ lib/pages/home/home_screen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:test/core/providers/provider.dart';
-import 'package:test/core/responses/fake_store_api_mode.dart';
-import 'package:test/core/services/fake_store_api.dart';
 import 'package:test/shared/widgets/item_card.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -13,25 +11,6 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  // List<FakeStoreResponse> fakeStoreData = [];
-  // var isLoaded = false;
-
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   getFakeStore();
-  // }
-
-  // getFakeStore() async {
-  //   fakeStoreData = await FakeStoreApi().getData();
-
-  //   if (fakeStoreData.isNotEmpty) {
-  //     setState(() {
-  //       isLoaded = true;
-  //     });
-  //   }
-  // }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(body: Consumer(builder: (context, ref, child) {
@@ -55,22 +34,6 @@ class _HomeScreenState extends State<HomeScreen> {
           child: CircularProgressIndicator(),
         ),
       );
-      // return Visibility(
-      //   visible: isLoaded,
-      //   replacement: const Center(
-      //     child: CircularProgressIndicator(),
-      //   ),
-      //   child: Center(
-      //     child: GridView.count(
-      //       crossAxisCount: 2,
-      //       children: fakeStoreData
-      //           .map((item) => ItemCard(
-      //                 item: item,
-      //               ))
-      //           .toList(),
-      //     ),
-      //   ),
-      // );
     }));
   }
 }
